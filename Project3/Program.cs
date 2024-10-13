26bfa547be80fe30cb7dce158450e1bf7ee44c50
fairraven31 <thebassawaddy@gmail.com>
2024-10-13 13:16:47 +0000

    fixed project structure errors

---
+++ Project3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //declaring variables
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("What is your name");
+            Console.Write("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            Console.Write("Type your last Name: ");
+            string myLastName;
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+            Console.ReadLine();
+    }
+}
