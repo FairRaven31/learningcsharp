12d50307635b38ff37217d97fda12746c383371d
fairraven31 <thebassawaddy@gmail.com>
2024-10-13 13:22:37 +0000

    fixed indentation in files

--- Project3/Program.cs
+++ Project3/Program.cs
@@ -12,26 +12,26 @@ class Program
     static void Main(string[] args)
     {
         //declaring variables
-            /*
-            int x;
-            int y;
+        /*
+        int x;
+        int y;
 
-            x = 7;
-            y = x + 3;
-            Console.WriteLine(y);
-            Console.ReadLine();
-            */
+        x = 7;
+        y = x + 3;
+        Console.WriteLine(y);
+        Console.ReadLine();
+        */
 
-            Console.WriteLine("What is your name");
-            Console.Write("Type your first name: ");
-            string myFirstName;
-            myFirstName = Console.ReadLine();
+        Console.WriteLine("What is your name");
+        Console.Write("Type your first name: ");
+        string myFirstName;
+        myFirstName = Console.ReadLine();
 
-            Console.Write("Type your last Name: ");
-            string myLastName;
-            myLastName = Console.ReadLine();
+        Console.Write("Type your last Name: ");
+        string myLastName;
+        myLastName = Console.ReadLine();
 
-            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
-            Console.ReadLine();
+        Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+        Console.ReadLine();
     }
 }
